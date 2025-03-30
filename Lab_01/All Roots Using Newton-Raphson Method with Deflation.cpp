819dbb8c6028ae656102497563c9ec3b02fa9958
FaisalHoqueRifat <108684295+FaisalHoqueRifat@users.noreply.github.com>
2025-03-31 03:07:43 +0600

    Lab-1 Q-10 Bug Fixed

--- Lab_01/All Roots Using Newton-Raphson Method with Deflation.cpp
+++ Lab_01/All Roots Using Newton-Raphson Method with Deflation.cpp
@@ -1,54 +1,106 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-double f(double x)
+double evaluatePolynomial(const vector<double>& coeffs, double x) 
 {
-    return x * x * x - 6 * x + 4;
-}
-
-double f_prime(double x)
-{
-    return 3 * x * x - 6;
-}
-
-double newtonRaphson(double x0, double tol)
-{
-    double x1;
-    while (true)
+    double result = 0.0;
+    for (int i = 0; i < coeffs.size(); i++) 
     {
-        x1 = x0 - f(x0) / f_prime(x0);
-        if (fabs(x1 - x0) < tol) break;
-        x0 = x1;
+        result += coeffs[i] * pow(x, coeffs.size() - 1 - i);
     }
-    return x1;
+    return result;
 }
 
-vector<double> deflatePolynomial(vector<double> coeffs, double root)
+double evaluateDerivative(const vector<double>& coeffs, double x)
+{
+    double result = 0.0;
+    for (int i = 0; i < coeffs.size() - 1; i++) 
+    {
+        int power = coeffs.size() - 1 - i;
+        result += power * coeffs[i] * pow(x, power - 1);
+    }
+    return result;
+}
+
+vector<double> syntheticDivision(const vector<double>& coeffs, double root) 
 {
     vector<double> newCoeffs(coeffs.size() - 1);
     newCoeffs[0] = coeffs[0];
-    for (double i = 1; i < newCoeffs.size(); i++) 
+    
+    for (int i = 1; i < newCoeffs.size(); i++)
     {
-        newCoeffs[i] = coeffs[i] + newCoeffs[i - 1] * root;
+        newCoeffs[i] = newCoeffs[i-1] * root + coeffs[i];
     }
+    
     return newCoeffs;
 }
 
-int main() 
+double newtonRaphson(const vector<double>& coeffs, double x0, double E) 
 {
-    double tol = 0.001;
-    vector<double> coeffs = {1, 0, -6, 4}; // x^3 - 6x + 4
+    double xr = x0;
+    double prev_xr;
+    int iterations = 0;
+    const int max_iterations = 100;
+    
+    while (fabs(xr - prev_xr) > E)
+    {
+        prev_xr = xr;
+        double fx = evaluatePolynomial(coeffs, xr);
+        double dfx = evaluateDerivative(coeffs, xr);
+        
+        if (fabs(dfx) < 1e-10) 
+        { 
+            cout << "Derivative too small. Trying a different initial guess." << endl;
+            xr += 0.5;
+            continue;
+        }
+        
+        xr = xr - fx / dfx;
+        iterations++;
+        
+        if (iterations > max_iterations) 
+        {
+            cout << "Maximum iterations reached." << endl;
+            break;
+        }
+    } 
+    
+    return xr;
+}
 
-    double root1 = newtonRaphson(1.5, tol);
-    cout << "Root 1: " << root1 << endl;
+int main()
+{
+    // Original polynomial: x^3 - 6x + 4 = 0
+    vector<double> coefficients = {1, 0, -6, 4};
+    
+    double E = 0.001; 
+    double x0 = 1.0;  
+    
+    vector<double> roots;
+    vector<double> currentCoeffs = coefficients;
+    int n = currentCoeffs.size() - 1;
+    
+    cout << fixed << setprecision(3);
+    
+    while (n > 1) 
+    {
+        double root = newtonRaphson(currentCoeffs, x0, E);
+        roots.push_back(root);
+        
+        currentCoeffs = syntheticDivision(currentCoeffs, root);
+        n = currentCoeffs.size() - 1;
+        
+        x0 = root;
+    }
+    
+    double lastRoot = -currentCoeffs[1] / currentCoeffs[0];
+    roots.push_back(lastRoot);
 
-    coeffs = deflatePolynomial(coeffs, root1);
-    double root2 = newtonRaphson(1.5, tol);
-    cout << "Root 2: " << root2 << endl;
-
-    coeffs = deflatePolynomial(coeffs, root2);
-    double root3 = newtonRaphson(1.5, tol);
-    cout << "Root 3: " << root3 << endl;
-
+    cout << "Roots of the polynomial x^3 - 6x + 4 = 0 are:" << endl;
+    for (int i = 0; i < roots.size(); ++i)
+    {
+        cout << "Root " << i+1 << ": " << roots[i] << endl;
+    }
+    
     return 0;
 }
