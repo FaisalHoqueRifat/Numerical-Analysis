1517bc554b778cec2b46f9b894b1e4cdefbc4d0d
FaisalHoqueRifat <108684295+FaisalHoqueRifat@users.noreply.github.com>
2025-04-28 23:47:07 +0600

    Round Off Bug Fixed

--- Lab_01/Round Off a Number with n Significant Figures Using Banker's Rule.cpp
+++ Lab_01/Round Off a Number with n Significant Figures Using Banker's Rule.cpp
@@ -1,27 +1,99 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-double roundToSignificantFigures(double num, int n)
+vector<int> getSignificantIndexes(string number, int n) 
 {
-    if (num == 0.0) return 0.0;
-
-    double d = ceil(log10(num < 0 ? -num : num));
-    int power = n - static_cast<int>(d);
-
-    double magnitude = pow(10.0, power);
-    long shifted = round(num * magnitude);
-
-    return shifted / magnitude;
+    vector<int> indexes;
+    bool significantEncountered = false;
+    for (int i = 0; i < number.size(); i++) 
+    {
+        if (isdigit(number[i])) 
+        {
+            if (number[i] != '0' || significantEncountered) 
+            {
+                significantEncountered = true;
+                indexes.push_back(i);
+                if (indexes.size() == n)
+                    break;
+            }
+        }
+    }
+    return indexes;
 }
 
-int main()
+string roundToNSignificant(string number, int n)
 {
-    double number;
+
+    vector<int> sigIdx = getSignificantIndexes(number, n+1);
+
+    if (sigIdx.size() < n)
+        return number;
+
+    string result = number;
+
+    if (sigIdx.size() == n)
+        return number.substr(0, sigIdx[n-1]+1);
+
+    int nthIdx = sigIdx[n-1];
+    int n1thIdx = sigIdx[n];
+
+    char nthVal = number[nthIdx];
+    char n1thVal = number[n1thIdx];
+
+    bool roundUp = false;
+
+    if (n1thVal < '5')
+        roundUp = false;
+    else if (n1thVal > '5')
+        roundUp = true;
+    else 
+    {
+        if ((nthVal - '0') % 2 == 0)
+            roundUp = false;
+        else
+            roundUp = true;
+    }
+
+    string rounded = number.substr(0, nthIdx+1);
+
+    if (roundUp) 
+    {
+        int i = nthIdx;
+        while (i >= 0) 
+        {
+            if (!isdigit(rounded[i])) 
+            {
+                i--;
+                continue;
+            }
+            if (rounded[i] < '9') 
+            {
+                rounded[i] += 1;
+                break;
+            } 
+            else
+            {
+                rounded[i] = '0';
+                i--;
+            }
+        }
+        if (i < 0)
+            rounded = "1" + rounded;
+    }
+    return rounded;
+}
+
+int main() 
+{
+    string number;
     int n;
     cout << "Enter a number: ";
     cin >> number;
-    cout << "Enter number of significant figures: ";
+    cout << "Enter n (significant digits to keep): ";
     cin >> n;
-    cout << "Rounded number: " << roundToSignificantFigures(number, n) << endl;
+
+    string res = roundToNSignificant(number, n);
+    cout << "Rounded to " << n << " significant digits: " << res << endl;
+
     return 0;
 }
